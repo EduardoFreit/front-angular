64b8e60c67f054b64ac89d7466189db17935a538
lepf <lepf@discente.ifpe.edu.br>
2026-03-01 09:18:40 -0300

    ajustes nas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './screens/login/login.component';
 import { DashboardComponent } from './screens/dashboard/dashboard.component';
-import { DevelopersComponent } from './screens/developers/developers.component';
-import { ProjectsComponent } from './screens/projects/projects.component';
-import { AllocationsComponent } from './screens/allocations/allocations.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { AuthGuard } from './guards/auth.guard';
 import { PublicGuard } from './guards/public.guard';
+import { DevelopersComponent } from './screens/developers/developers.component';
+import { ProjectsComponent } from './screens/projects/projects.component';
+import { AllocationsComponent } from './screens/allocations/allocations.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [PublicGuard] },
